9cc7b362447c6d860e5db79596acb7b728b428a2
Dmitry Arshinkin <arshinkinda@gmail.com>
2021-09-06 23:42:25 +0300

    checkout-function: added to handler SNS processing

--- checkout-function/src/main/java/example/Handler.java
+++ checkout-function/src/main/java/example/Handler.java
@@ -1,65 +1,90 @@
 package example;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
-
-import software.amazon.awssdk.services.lambda.model.GetAccountSettingsRequest;
-import software.amazon.awssdk.services.lambda.model.GetAccountSettingsResponse;
-import software.amazon.awssdk.services.lambda.model.ServiceException;
-import software.amazon.awssdk.services.lambda.LambdaAsyncClient;
-import software.amazon.awssdk.services.lambda.model.AccountUsage;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.StringBuilder;
-import java.util.Map;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.lambda.LambdaAsyncClient;
+import software.amazon.awssdk.services.lambda.model.GetAccountSettingsRequest;
+import software.amazon.awssdk.services.lambda.model.GetAccountSettingsResponse;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
 
 // Handler value: example.Handler
-public class Handler implements RequestHandler<SQSEvent, String>{
-  private static final Logger logger = LoggerFactory.getLogger(Handler.class);
-  private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-  private static final LambdaAsyncClient lambdaClient = LambdaAsyncClient.create();
-  public Handler(){
-    CompletableFuture<GetAccountSettingsResponse> accountSettings = lambdaClient.getAccountSettings(GetAccountSettingsRequest.builder().build());
-    try {
-      GetAccountSettingsResponse settings = accountSettings.get();
-    } catch(Exception e) {
-      e.getStackTrace();
+public class Handler implements RequestHandler<SQSEvent, String> {
+    private static final Logger logger = LoggerFactory.getLogger(Handler.class);
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final LambdaAsyncClient lambdaClient = LambdaAsyncClient.create();
+    private static final Region SNS_REGION = Region.US_EAST_1;
+    public static final String SNS_TARGET_ARN = "arn:aws:sns:us-east-1:310213316965:shop-order";
+    public static final String SNS_NEW_ORDER = "New order";
+    private final CompletableFuture<GetAccountSettingsResponse> accountSettings;
+    private final SnsClient snsClient;
+
+    public Handler() {
+        accountSettings = lambdaClient.getAccountSettings(GetAccountSettingsRequest.builder().build());
+        snsClient = SnsClient.builder()
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .region(SNS_REGION)
+                .build();
     }
-  }
-  @Override
-  public String handleRequest(SQSEvent event, Context context)
-  {
-    String response = new String();
-    // call Lambda API
-    logger.info("Getting account settings");
-    CompletableFuture<GetAccountSettingsResponse> accountSettings = 
-        lambdaClient.getAccountSettings(GetAccountSettingsRequest.builder().build());
-    // log execution details
-    logger.info("ENVIRONMENT VARIABLES: {}", gson.toJson(System.getenv()));
-    logger.info("CONTEXT: {}", gson.toJson(context));
-    logger.info("EVENT: {}", gson.toJson(event));
-    // process event
-    for(SQSMessage msg : event.getRecords()){
-      logger.info(msg.getBody());
+
+    @Override
+    public String handleRequest(SQSEvent event, Context context) {
+        String message = getEventMessage(event);
+        logging(event, context, message);
+        PublishRequest request = PublishRequest.builder()
+                .message(message)
+                .targetArn(SNS_TARGET_ARN)
+                .subject(SNS_NEW_ORDER)
+                .build();
+        PublishResponse responseSns = null;
+        try {
+            responseSns = snsClient.publish(request);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        String response = Optional.ofNullable(responseSns).map(PublishResponse::toString).orElse("nothing");
+        logger.info("RESPONSE: {}", response);
+        return response;
     }
-    // process Lambda API response
-    try {
-      GetAccountSettingsResponse settings = accountSettings.get();
-      response = gson.toJson(settings.accountUsage());
-      logger.info("Account usage: {}", response);
-    } catch(Exception e) {
-      e.getStackTrace();
+
+    private void logging(SQSEvent event, Context context, String message) {
+        // call Lambda API
+        logger.info("Getting account settings");
+        // log execution details
+        logger.info("ENVIRONMENT VARIABLES: {}", gson.toJson(System.getenv()));
+        logger.info("CONTEXT: {}", gson.toJson(context));
+        logger.info("EVENT: {}", gson.toJson(event));
+        logger.info(message);
+        // process event
+
+        // process Lambda API response
+        try {
+            String accountUsage = gson.toJson(accountSettings.get().accountUsage());
+            logger.info("Account usage: {}", accountUsage);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
-    return response;
-  }
+
+    private String getEventMessage(SQSEvent event) {
+        StringBuilder message = new StringBuilder();
+        for (SQSMessage msg : event.getRecords()) {
+            message.append(msg.getBody());
+        }
+        return message.toString();
+    }
 }
--- checkout-function/src/test/java/example/InvokeTest.java
+++ checkout-function/src/test/java/example/InvokeTest.java
@@ -1,71 +1,59 @@
 package example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.List;
-import java.util.ArrayList;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.strategy.sampling.NoSamplingStrategy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 class InvokeTest {
-  private static final Logger logger = LoggerFactory.getLogger(InvokeTest.class);
-  Gson gson = new GsonBuilder()
-          .registerTypeAdapter(SQSEvent.class, new SQSEventDeserializer())
-          .setPrettyPrinting()
-          .create();
+    private static final Logger logger = LoggerFactory.getLogger(InvokeTest.class);
+    Gson gson = new GsonBuilder()
+            .registerTypeAdapter(SQSEvent.class, new SQSEventDeserializer())
+            .setPrettyPrinting()
+            .create();
 
-  public InvokeTest() {
-    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard();
-    builder.withSamplingStrategy(new NoSamplingStrategy());
-    AWSXRay.setGlobalRecorder(builder.build());
-  }
+    public InvokeTest() {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard();
+        builder.withSamplingStrategy(new NoSamplingStrategy());
+        AWSXRay.setGlobalRecorder(builder.build());
+    }
 
-  @Test
-  void invokeTest() {
-    AWSXRay.beginSegment("checkout-function-test");
-    String path = "src/test/resources/event.json";
-    String eventString = loadJsonFile(path);
-    SQSEvent event = gson.fromJson(eventString, SQSEvent.class);
-    Context context = new TestContext();
-    String requestId = context.getAwsRequestId();
-    Handler handler = new Handler();
-    String result = handler.handleRequest(event, context);
-    assertTrue(result.contains("totalCodeSize"));
-    AWSXRay.endSegment();
-  }
+    @Test
+    void invokeTest() {
+        AWSXRay.beginSegment("checkout-function-test");
+        String path = "src/test/resources/event.json";
+        String eventString = loadJsonFile(path);
+        SQSEvent event = gson.fromJson(eventString, SQSEvent.class);
+        Context context = new TestContext();
+        String requestId = context.getAwsRequestId();
+        Handler handler = new Handler();
+        String result = handler.handleRequest(event, context);
+        assertNotNull(result);
+        AWSXRay.endSegment();
+    }
 
-  private static String loadJsonFile(String path)
-  {
-      StringBuilder stringBuilder = new StringBuilder();
-      try (Stream<String> stream = Files.lines( Paths.get(path), StandardCharsets.UTF_8))
-      {
-          stream.forEach(s -> stringBuilder.append(s));
-      }
-      catch (IOException e)
-      {
-          e.printStackTrace();
-      }
-      return stringBuilder.toString();
-  }
+    private static String loadJsonFile(String path) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> stringBuilder.append(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
 }
